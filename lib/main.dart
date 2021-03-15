cedba43dc6abbba6f6803052c864eccb28fa6321
xswapsx <xswapsx@gmail.com>
2021-03-15 13:40:51 +0530

    "Created StatefulWidget called RandomWords"

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
-      title: 'WelcomeToListApp',
+      title: 'ListApp',
+      theme: ThemeData(primarySwatch: Colors.green),
       home: Scaffold(
-        appBar: AppBar(title: Text('List App')),
+        appBar: AppBar(
+          title: Text('List App'),
+        ),
         body: Center(
-          child: Text('Welcome Ma G'),
+          child: Text(wordPair.asPascalCase),
         ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
